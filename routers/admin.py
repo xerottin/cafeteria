2272e17ecce9a01ee3ccb8405d166b78942c4a1f
xerottin <xerottin@gmail.com>
2024-11-11 16:28:51 +0500

    changed pk: int to pk=..._data.id

--- routers/admin.py
+++ routers/admin.py
@@ -1,7 +1,8 @@
-from fastapi import APIRouter
+from fastapi import APIRouter, Security
 from fastapi.params import Depends
 from sqlalchemy.orm import Session
 
+from auth.oauth2 import get_current_admin
 from database import db_admin
 from schemas.admin import AdminCreate, AdminInDB, AdminResponse, AdminUpdate
 from database.base import get_pg_db
@@ -14,17 +15,20 @@ async def create_admin(admin: AdminCreate, db: Session = Depends(get_pg_db)):
     return db_admin.create_admin(db, admin)
 
 @router.get("/", response_model=AdminInDB)
-async def get_admin(pk:int, db: Session = Depends(get_pg_db)):
+async def get_admin(db: Session = Depends(get_pg_db), admin_data=Security(get_current_admin)):
+    pk=admin_data.id
     return db_admin.get_admin(pk, db)
 
 @router.put("/", response_model=AdminInDB)
 async def put_admin(
-    pk: int,
     admin_update: AdminUpdate,
+    admin_data=Security(get_current_admin),
     db: Session = Depends(get_pg_db)
 ):
+    pk=admin_data.id
     return db_admin.update_admin(db, pk, admin_update)
 
 @router.delete("/", response_model=AdminInDB)
-async def delete_admin(pk:int, db: Session = Depends(get_pg_db)):
+async def delete_admin(db: Session = Depends(get_pg_db), admin_data=Security(get_current_admin)):
+    pk=admin_data.id
     return db_admin.delete_admin(pk, db)
