7e78e50e9a0fe471c1b382a89b3e2bcdccdc9cc8
xerottin <xerottin@gmail.com>
2024-11-19 09:58:53 +0500

    first thinks

--- database/base.py
+++ database/base.py
@@ -4,6 +4,7 @@ from sqlalchemy import create_engine
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import Session, sessionmaker
 from settings import DATABASE_URL
+import redis
 
 Base = declarative_base()
 
@@ -26,4 +27,11 @@ def get_pg_db() -> Iterator[Session]:
     finally:
         db.close()
 
+redis_client = redis.StrictRedis(host="localhost", port=6379, db=0, decode_responses=True)
 
+async def check_redis_connection():
+    try:
+        await redis_client.ping()
+        print("Connected to Redis")
+    except redis.ConnectionError:
+        print("Failed to connect to Redis")
--- test.py
+++ test.py
@@ -1,9 +1,25 @@
-from sqlalchemy.orm import Session
+def archive_order(order_id: int, db: Session):
+    # Получение заказа из базы данных
+    order = db.query(Order).filter(Order.id == order_id).first()
 
-from database.db_user import get_user_archive, archive_order
+    if not order:
+        raise ValueError("Order not found")
 
-with Session() as db:
-    archive_order(order_id=1, db=db)
+    if  not order.status :  # Проверяем, что заказ неактивен
+        # Формируем данные для архива
+        order_data = {
+            "order_id": order.id,
+            "cafeteria_id": order.cafeteria_id,
+            "user_id": order.user_id,
+            "order_items": [
+                {"coffee_id": item.coffee_id, "quantity": item.quantity}
+                for item in order.order_items
+            ]
+        }
 
-user_archive = get_user_archive(user_id=1)
-print(user_archive)
+        # Сохраняем данные в Redis
+        redis_client.rpush(f"user:{order.user_id}:archives", json.dumps(order_data))
+
+        print(f"Order {order.id} archived for user {order.user_id}.")
+    else:
+        print(f"Order {order.id} is still active and cannot be archived.")
